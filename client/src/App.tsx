00b78e3e0ff1dc5dc37da67cd901e0a200df6d84
Alexis <dnc.alexis@gmail.com>
2024-02-26 15:01:08 +0100

    Add components + pages from designs & refactor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,21 @@
 import "./App.css"
 import Nav from "./components/Nav/Nav"
-import Hero from "./components/Hero/Hero"
-import Features from "./components/Features/Features"
+// import Hero from "./components/Hero/Hero"
+// import Features from "./components/Features/Features"
 import Footer from "./components/Footer/Footer"
+import User from "./pages/auth/User/User"
+// import SignIn from "./pages/public/SignIn/SignIn"
 function App() {
   return (
     <>
-      <header>
-        <Nav />
-      </header>
-      <main>
-        <Hero />
-        <Features />
+      {/* <header> */}
+      <Nav />
+      {/* </header> */}
+      <main className="main bg-dark">
+        {/* <Hero />
+        <Features /> */}
+        {/* <SignIn /> */}
+        <User />
       </main>
       <Footer />
     </>
